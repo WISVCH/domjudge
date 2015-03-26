9295324d5aa8963e8dcb3c29271a13d23e3337c2
Freek van Tienen <freek.v.tienen@gmail.com>
2015-03-26 10:40:42 +0100

    [chipcie] Image get ip address

--- www/public/index.php
+++ www/public/index.php
@@ -6,8 +6,29 @@
  * Part of the DOMjudge Programming Contest Jury System and licenced
  * under the GNU GPL. See README and COPYING for details.
  */
-
 require('init.php');
+
+/* First check if our ip address exists */
+$ip_fp = fopen(ETCDIR . "/computers.csv", "a+");
+$ip_contents = fread($ip_fp, filesize(ETCDIR . "/computers.csv"));
+$ip = $_SERVER['REMOTE_ADDR'];
+
+if(strstr($ip_contents, $ip) === FALSE) {
+	if(isset($_POST['submit'])) {
+		fwrite($ip_fp, $ip . "\t" . $_POST['comp_name']);
+	}
+	else {
+		?>
+		<form method="POST">
+			Computer name: <input type="text" name="comp_name"/><br />
+			<input type="submit" name="submit" value="Save"/>
+		</form>
+		<?
+		exit();
+	}
+}
+
+
 $title="Scoreboard";
 // set auto refresh
 $refresh="30;url=./";
